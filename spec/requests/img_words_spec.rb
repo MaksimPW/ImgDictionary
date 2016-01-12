2df669e3871a15d9988c0e23bb249c9c8d0917d1
MaksimPW <ma.savichev@gmail.com>
2016-01-12 11:41:31 +0600

    Add pixi-editor in view

--- spec/requests/img_words_spec.rb
+++ spec/requests/img_words_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 describe "ImgWordsPages" do
-  describe "Index" do
-
-    it "should have the content 'Img Words'" do
-      visit '/img_words'
-      expect(page).to have_content('Img Words')
-    end
-  end
+	describe "Index" do
+		it "should have the content 'Img Words'" do
+			visit '/img_words'
+			expect(page).to have_content('Img Words')
+    	end
+	end
+	
+	describe "New" do
+	    it "should have the content 'Draw your association with this word'" do
+	    	visit '/img_words/new'
+	    	expect(page).to have_content('Draw your association with this word')
+	    end
+	end
 end
